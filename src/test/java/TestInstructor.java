1d4e7216e9450f9dfb226085537984ed18554b19
vjcho <vjcho@ucdavis.edu>
2017-02-27 23:41:05 -0800

    Added test classes

---
+++ src/test/java/TestInstructor.java
@@ -0,0 +1,31 @@
+import api.IAdmin;
+import api.core.impl.Admin;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by mushrumi on 2/27/2017.
+ */
+public class TestInstructor {
+    private IAdmin admin;
+
+    @Before
+    public void setup() {
+        this.admin = new Admin();
+    }
+
+    @Test
+    public void testMakeClass() {
+        this.admin.createClass("Test", 2017, "Instructor", 15);
+        assertTrue(this.admin.classExists("Test", 2017));
+    }
+
+    @Test
+    public void testMakeClass2() {
+        this.admin.createClass("Test", 2016, "Instructor", 15);
+        assertFalse(this.admin.classExists("Test", 2016));
+    }
+}
